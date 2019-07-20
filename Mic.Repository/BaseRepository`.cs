e7662bc7e1b077af6a4d4af6e09786c5b15cb443
Any <Any@ANY>
2019-07-20 19:12:53 +0400

    Some Changes

--- Mic.Repository/BaseRepository`.cs
+++ Mic.Repository/BaseRepository`.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Reflection;
 using System.Text;
 
 namespace Mic.Repository
--- Mic.Repository/Consts/Queries.cs
+++ Mic.Repository/Consts/Queries.cs
@@ -2,11 +2,13 @@
 {
     public static class Queries
     {
-        public const string SelectAll = "Select * From {0}";
-        public const string Select = "Select * From {0} Where {1}";
-        public const string SelectOne = "Select * From {0} Where {1} = {2}";
+        public const string SelectAll = "SELECT * FROM {0}";
+        public const string Select = "SELECT * FROM {0} WHERE {1}";
+        public const string SelectOne = "SELECT * FROM {0} WHERE {1} = {2}";
         public const string Insert = "INSERT INTO {0}({1}) VALUES({2})";
         public const string InsertScalar = "INSERT INTO {0}({1}) VALUES({2})" + "SELECT CAST(scope_identity() AS int)";
-        public const string Where = "Where";
+        public const string Update = "UPDATE {0} SET {1}={2} WHERE Id = {3}";
+        public const string Delete = "DELETE FROM {2} WHERE {1}";
+        //public const string Where = "WHERE";
     }
 }
